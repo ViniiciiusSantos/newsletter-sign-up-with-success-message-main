c3095d2f7a27f85c79537c92cde8fb28e20dab89
ViniciiusCampos <134455972+ViniiciiusSantos@users.noreply.github.com>
2023-06-24 09:26:40 -0300

    finalizando o desktop

--- script/script.js
+++ script/script.js
@@ -3,20 +3,17 @@ let form = document.querySelector('.form');
 let small = document.querySelector('.smalll');
 let validado = document.querySelector('.validado')
 
-if (emailOut && form && small && validado) {
-    form.addEventListener('submit', (e) => {
-      if (validacao(emailOut.value)) {
-        validado.style.display = 'block';
-        small.style.display = 'none';
-      } else {
-        small.style.display = 'block';
-        validado.style.display = 'none';
-      }
-      e.preventDefault();
-    });
+
+form.addEventListener('submit' , (e)=>{
+  if(validacao(emailOut.value) == true) {
+      validado.style.display = 'block';
+  } else {
+      small.style.display = 'block'
   }
-  
-  function validacao(email) {
-    var emailPadrao = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return emailPadrao.test(email);
-  }
+  e.preventDefault()
+})
+
+function validacao(email) {
+  var emailPadrao = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return emailPadrao.test(email);
+}
