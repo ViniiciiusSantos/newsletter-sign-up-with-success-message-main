63b2df7fbc7c5646e3372cf4031b49f4af6bdab0
ViniciiusCampos <134455972+ViniiciiusSantos@users.noreply.github.com>
2023-06-30 08:54:05 -0300

    email arrumado.

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ const small = document.querySelector('.smalll');
 const validado = document.querySelector('.validado');
 const containerP = document.querySelector('.container-principal');
 const dimiss = document.querySelector('.button-confirm');
+const strongOut = document.querySelector('strong');
 
 
 emailOut.addEventListener('keyup', () => {
@@ -23,6 +24,8 @@ function validacaoEmail(emailOut) {
 
 form.addEventListener('submit' , (e) => {
   e.preventDefault();
+  const emailV = emailOut.value;
+  strongOut.textContent = emailV;
   containerP.style.display = "none"
   validado.style.display = "block"
 })
